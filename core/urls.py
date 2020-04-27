bde0443d9e182d296e8ed222fb90bb50ac5a8f76
Mateusz Smolik <m.smolik@hotmail.com>
2020-04-27 20:04:37 +0200

    Added required styles and images. First page.

--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from donation.views import IndexPage
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexPage.as_view())
 ]
