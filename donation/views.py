09fc05cf6b0a55ab637eb88a08713bd3f763a04a
Marcin Dylong <marcindylong@gmail.com>
2020-04-28 19:49:48 +0200

    Paginacja

--- donation/views.py
+++ donation/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
@@ -7,11 +8,24 @@ from .models import Category,Institution,Donation
 
 class IndexPage(View):
     def get(self, request):
+        ## Liczniki
         qty = Donation.objects.values('quantity').aggregate(Sum('quantity'))
         inst = Donation.objects.values('institution').distinct().count()
+        ## Fundację
         fund = Institution.objects.filter(type='F')
+        paginator_f = Paginator(fund,2)
+        page_f = request.GET.get('page_f')
+        fund = paginator_f.get_page(page_f)
+        ## Organizacje pozarządowe
         orgp = Institution.objects.filter(type='OP')
+        paginator_o = Paginator(orgp, 2)
+        page_o = request.GET.get('page_o')
+        orgp = paginator_o.get_page(page_o)
+        ## Zbiórki lokalne
         zblk = Institution.objects.filter(type='ZL')
+        paginator_z = Paginator(zblk, 2)
+        page_z = request.GET.get('page_z')
+        zblk = paginator_z.get_page(page_z)
         ctx= {'qty': qty['quantity__sum'], 'inst': inst, 'fund': fund, 'orgp': orgp, 'zblk': zblk}
         return render(request, 'index.html', ctx)
 
