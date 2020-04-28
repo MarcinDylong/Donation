b151f9af2e2caca9eb4f201fb3b9b27f4343a000
Marcin Dylong <marcindylong@gmail.com>
2020-04-28 22:48:13 +0200

    Tworzenie użytkownika

--- donation/views.py
+++ donation/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.models import User
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import TemplateView
 from django.db.models import Sum
+from django.contrib import messages
+from braces.views import CsrfExemptMixin
 
 from .models import Category,Institution,Donation
 
@@ -26,6 +29,7 @@ class IndexPage(View):
         paginator_z = Paginator(zblk, 2)
         page_z = request.GET.get('page_z')
         zblk = paginator_z.get_page(page_z)
+        ## Context
         ctx= {'qty': qty['quantity__sum'], 'inst': inst, 'fund': fund, 'orgp': orgp, 'zblk': zblk}
         return render(request, 'index.html', ctx)
 
@@ -36,10 +40,53 @@ class AddDonation(View):
     def get(self, request):
         return render(request, 'form.html')
 
-class Login(View):
+class Login(CsrfExemptMixin, View):
     def get(self, request):
         return render(request, 'login.html')
 
-class Register(View):
+class Register(CsrfExemptMixin, View):
+    def password_valid(self, passwd):
+        errors = []
+        if len(passwd) < 8:
+            errors.append('Hasło musi mieć przynajmniej 8 znaków!')
+
+        if not any(char.isdigit() for char in passwd):
+            errors.append('Hasło musi zawierać przynajmniej jedną cyfrę!')
+
+        if not any(char.isupper() for char in passwd):
+            errors.append('Hasło musi zawierać przynajmniej jedną dużą literę!')
+
+        if not any(char.islower() for char in passwd):
+            errors.append('Hasło musi zawierać przynajmniej jedną małą literę!')
+
+        return errors
+
     def get(self, request):
         return render(request, 'register.html')
+
+    def post(self, request):
+        name = request.POST.get('name')
+        surname = request.POST.get('surname')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+
+        errors = self.password_valid(password)
+        if errors:
+            for e in errors:
+                messages.error(request, e)
+            return render(request, 'register.html')
+
+        if password != password2:
+            messages.error(request,'Powtórzone hasło nie zgadza się!')
+            return render(request, 'register.html')
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'Użytkownik o takim e-mailu już istnieje')
+            return render(request, 'register.html')
+
+
+
+        User.objects.create_user(first_name=name, last_name=surname, username=email, email=email, password=password)
+        return render(request, 'login.html')
+
