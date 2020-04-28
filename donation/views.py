c4f58bc239df735a4e7813d8402ab1db4207429c
Marcin Dylong <marcindylong@gmail.com>
2020-04-28 16:16:38 +0200

    Dynamiczne statystyki w index.html

--- donation/views.py
+++ donation/views.py
@@ -1,10 +1,18 @@
+from itertools import count
+
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
+from django.db.models import Sum
+
+from .models import Category,Institution,Donation
 
 class IndexPage(View):
     def get(self, request):
-        return render(request, 'index.html')
+        bags = Donation.objects.values('quantity').aggregate(Sum('quantity'))
+        inst_qty = Donation.objects.values('institution').distinct().count()
+        ctx= {'qty': bags['quantity__sum'], 'inst': inst_qty}
+        return render(request, 'index.html', ctx)
 
 # class IndexPage(TemplateView):
 #     template_name = 'index.html'
