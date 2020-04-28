c3aedc4e9253ac8ce52fbcdced4efca81f65f682
Marcin Dylong <marcindylong@gmail.com>
2020-04-28 15:21:46 +0200

    Dodano modele Category, Institution, Donation

--- donation/models.py
+++ donation/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+Institution_type = [
+    ('F', 'Fundacja'),
+    ('OP', 'Organizacja porządkowa'),
+    ('ZL', 'Zbiórka lokalna'),
+]
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    type = models.CharField(max_length=2, choices=Institution_type, default='Fundacja')
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.name
+
+
+class Donation(models.Model):
+    quantity = models.SmallIntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=16)
+    city = models.CharField(max_length=32)
+    zip_code = models.CharField(max_length=8)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
