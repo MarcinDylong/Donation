c6900be0fd021763784035466b685db3032f7819
Marcin Dylong <marcindylong@gmail.com>
2021-02-02 16:52:28 +0100

    Prevent deleting superuser

--- donation/admin.py
+++ donation/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.db.models.signals import pre_delete
+from django.dispatch.dispatcher import receiver
+from django.contrib.auth.models import User
+from django.core.exceptions import PermissionDenied
 
 from .models import Category, Institution, Donation
 
@@ -7,3 +11,8 @@ from .models import Category, Institution, Donation
 admin.site.register(Category)
 admin.site.register(Institution)
 admin.site.register(Donation)
+
+@receiver(pre_delete, sender=User)
+def delete_user(sender, instance, **kwargs):
+    if instance.is_superuser:
+        raise PermissionDenied
