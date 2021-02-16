f4966651adc636078614bf8cb5530720f6442787
Marcin Dylong <marcindylong@gmail.com>
2021-02-16 14:37:39 +0100

    Simple contact validator

--- donation/static/js/app.js
+++ donation/static/js/app.js
@@ -221,7 +221,6 @@ document.addEventListener("DOMContentLoaded", function() {
     updateForm() {
       this.$step.innerText = this.currentStep;
 
-      // TODO: Validation
 
       this.slides.forEach(slide => {
         slide.classList.remove("active");
@@ -234,16 +233,10 @@ document.addEventListener("DOMContentLoaded", function() {
       this.$stepInstructions[0].parentElement.parentElement.hidden = this.currentStep >= 6;
       this.$step.parentElement.hidden = this.currentStep >= 6;
 
-      // TODO: get data from inputs and show them in summary
     }
 
-    /**
-     * Submit form
-     *
-     * TODO: validation, send data to server
-     */
+
     submit(e) {
-      // e.preventDefault();
       this.currentStep++;
       this.updateForm();
     }
